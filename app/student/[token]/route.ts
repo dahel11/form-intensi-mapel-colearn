4c13fc94552bcbc7fc44444288efc21e62ed050c
dahel11 <dahlia.yanuarti@colearn.id>
2026-05-13 17:23:53 +0700

    Delete app/student/[token] directory

--- app/student/[token]/route.ts
+++
@@ -1,7 +0,0 @@
-// MOCK — hapus ini dan ganti dengan versi asli setelah Apps Script siap
-export async function GET() {
-  return Response.json({
-    studentname: 'Andi Prasetyo',
-    next_grade: 9,
-  });
-}
